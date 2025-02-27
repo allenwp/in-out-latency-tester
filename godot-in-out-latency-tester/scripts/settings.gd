4de9492217a49a0bc8adee26520996bdb084390e
Allen Pestaluky <allenpestaluky@gmail.com>
2025-02-27 15:40:40 -0500

    Added option to toggle vsync

--- godot-in-out-latency-tester/scripts/settings.gd
+++ godot-in-out-latency-tester/scripts/settings.gd
@@ -6,6 +6,13 @@ func _process(delta: float) -> void:
 	var isCapped = (Engine.max_fps > 0);
 	var capTarget = Engine.physics_ticks_per_second;
 	
+	
+	if Input.is_action_just_pressed("toggle_vsync"):
+		if DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		else:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		match DisplayServer.window_get_mode():
 			DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, DisplayServer.WINDOW_MODE_FULLSCREEN:
